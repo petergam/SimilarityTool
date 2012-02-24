b5a6e169f4252136fc80549eb75ec0c81ad9a059
Johan Van Beusekom <Johan@Johan-Van-Beusekoms-MacBook-Pro.local>
2012-02-24 20:19:29 +0100

    Utils mm

---
+++ src/Algorithms/LevenshteinDistance.java
@@ -0,0 +1,11 @@
+package Algorithms;
+
+public class LevenshteinDistance {
+
+	
+	public static int computeLevenshteinDistance(CharSequence str1,
+            CharSequence str2) {
+		
+		return 0;
+	}
+}
